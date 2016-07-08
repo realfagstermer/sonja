bb925f082af1a8889b88e4e629900ee9f98bd890
Erik Winge <erik.winge@ub.uio.no>
2016-07-08 10:41:26 +0200

    fix IDs

--- SQL/groups.sql
+++ SQL/groups.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS groups ;
 DROP TABLE IF EXISTS group_memberships ;
 
-CREATE TABLE groups 
+CREATE TABLE groups
 ( group_id             SERIAL          PRIMARY KEY
 , vocab_id             CHAR(10)        NOT NULL      REFERENCES vocabularies(vocab_id)
 , created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE group_memberships 
+CREATE TABLE group_memberships
 ( group_membership_id  SERIAL          PRIMARY KEY
 , group_id             INT             NOT NULL      REFERENCES groups(concept_group_id)
 , concept_id           INT             NOT NULL      REFERENCES concepts(concept_id)
