a43787d645631e3bd4e68c73c4996f92a02cd312
Erik Winge <erik.winge@ub.uio.no>
2016-07-08 10:47:36 +0200

    fix group id

--- SQL/groups.sql
+++ SQL/groups.sql
@@ -9,7 +9,7 @@ CREATE TABLE groups
 
 CREATE TABLE group_memberships
 ( group_membership_id  SERIAL          PRIMARY KEY
-, group_id             INT             NOT NULL      REFERENCES groups(concept_group_id)
+, group_id             INT             NOT NULL      REFERENCES groups(group_id)
 , concept_id           INT             NOT NULL      REFERENCES concepts(concept_id)
 , created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
 , UNIQUE(group_id, concept_id)
