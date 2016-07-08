c2dfa744e156eb74c96e081e8a47a232099936d4
Erik Winge <erik.winge@ub.uio.no>
2016-07-08 10:15:26 +0200

    add uniqueness constraint groups

--- SQL/groups.sql
+++ SQL/groups.sql
@@ -5,7 +5,6 @@ CREATE TABLE groups
 ( group_id             SERIAL          PRIMARY KEY
 , vocab_id             CHAR(10)        NOT NULL      REFERENCES vocabularies(vocab_id)
 , created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
-, UNIQUE(group_id)
 );
 
 CREATE TABLE group_memberships 
@@ -13,5 +12,5 @@ CREATE TABLE group_memberships
 , group_id             INT             NOT NULL      REFERENCES groups(concept_group_id)
 , concept_id           INT             NOT NULL      REFERENCES concepts(concept_id)
 , created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
-, UNIQUE(group_membership_id)
+, UNIQUE(group_id, concept_id)
 );
