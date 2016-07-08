2d85050c54c5c2f93d9215c3586d7cf5307e4410
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-07-08 09:04:18 +0200

    Create groups.sql
    Added tables that can support future grouping concepts in concept groups,
    for instance main subject groups initially based on data from `concepts.used_by_libs`

---
+++ groups.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS groups ;
+DROP TABLE IF EXISTS group_memberships ;
+
+CREATE TABLE groups 
+( group_id             SERIAL          PRIMARY KEY
+, vocab_id             CHAR(10)        NOT NULL      REFERENCES vocabularies(vocab_id)
+, created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
+, UNIQUE(group_id)
+);
+
+CREATE TABLE group_memberships 
+( group_membership_id  SERIAL          PRIMARY KEY
+, group_id             INT             NOT NULL      REFERENCES groups(concept_group_id)
+, concept_id           INT             NOT NULL      REFERENCES concepts(concept_id)
+, created              TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
+, UNIQUE(group_membership_id)
+);
