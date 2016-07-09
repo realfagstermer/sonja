19135beb058bf9b2559bf223c20c2b9b9a5a8a52
Erik Winge <erik.winge@ub.uio.no>
2016-07-09 09:19:23 +0200

    forgot users.sql in 0492ccb

---
+++ sql/users.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+( user_id               SERIAL          PRIMARY KEY
+, created               TIMESTAMP(0)    NOT NULL      DEFAULT CURRENT_TIMESTAMP
+, created_by            INT                           REFERENCES users(user_id)
+, username              VARCHAR(20)     NOT NULL
+, domain                VARCHAR(100)    NOT NULL
+, name                  VARCHAR(100)
+, UNIQUE(username, domain)
+);
