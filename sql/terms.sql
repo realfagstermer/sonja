58488139576953660b9f0d462a0caef8f3632103
Erik Winge <erik.winge@ub.uio.no>
2016-07-14 09:45:24 +0200

    terms: make author optional and lexical value mandatory

--- sql/terms.sql
+++ sql/terms.sql
@@ -4,11 +4,11 @@ create table terms
 ( term_id 				serial 			primary key
 , term_created			timestamp(0)	not null	default CURRENT_TIMESTAMP
 , term_modified			timestamp(0)
-, term_created_by		INT             NOT NULL    REFERENCES users(user_id)
-, term_modified_by		INT             NOT NULL    REFERENCES users(user_id)
+, term_created_by		INT                         REFERENCES users(user_id)
+, term_modified_by		INT                         REFERENCES users(user_id)
 , status				char(10)		not null
 , concept_id			int				not null	references concepts(concept_id)
-, lexical_value			varchar(500)
+, lexical_value			varchar(500)    NOT NULL
 , lang_id				char(3)			not null	references languages(lang_id)
 , constraint  			valid_status	check(status in ('preferred', 'non-pref', 'hidden'))
 , unique (concept_id, lang_id, lexical_value)
