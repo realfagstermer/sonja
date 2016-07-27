84072aa8e9312c6f12f3dd81440d1a675b05432f
Erik Winge <erik.winge@ub.uio.no>
2016-07-27 12:11:23 +0200

    SQL: sequence numbers must be reset after import

---
+++ sql/reset_serials.sql
@@ -0,0 +1,3 @@
+﻿SELECT pg_catalog.setval(pg_get_serial_sequence('concepts', 'external_id'), MAX(external_id)) FROM concepts;
+SELECT pg_catalog.setval(pg_get_serial_sequence('concepts', 'concept_id'), MAX(concept_id)) FROM concepts;
+--SELECT pg_catalog.setval(pg_get_serial_sequence('terms', 'term_id'), MAX(term_id)) FROM terms;
