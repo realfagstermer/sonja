54a2a02a88097e36cb8e5f0534272da126881535
Erik Winge <erik.winge@ub.uio.no>
2016-08-16 11:44:25 +0200

    SQL: fix strings sequence number

--- sql/reset_serials.sql
+++ sql/reset_serials.sql
@@ -1,3 +1,4 @@
-﻿SELECT pg_catalog.setval(pg_get_serial_sequence('concepts', 'external_id'), MAX(external_id)) FROM concepts;
+SELECT pg_catalog.setval(pg_get_serial_sequence('concepts', 'external_id'), MAX(external_id)) FROM concepts;
 SELECT pg_catalog.setval(pg_get_serial_sequence('concepts', 'concept_id'), MAX(concept_id)) FROM concepts;
+SELECT pg_catalog.setval(pg_get_serial_sequence('strings', 'string_id'), MAX(string_id)) FROM strings;
 --SELECT pg_catalog.setval(pg_get_serial_sequence('terms', 'term_id'), MAX(term_id)) FROM terms;
