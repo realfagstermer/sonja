45246eb8ae532eb559d239f2cbd9fa70f663307d
Erik Winge <erik.winge@ub.uio.no>
2016-08-22 09:54:25 +0200

    make sure statements are closed

--- src/sonja/Database.java
+++ src/sonja/Database.java
@@ -98,86 +98,95 @@ public class Database implements AutoCloseable {
     }
 
     public void addTerm(Term concept, String lexicalValue, TermStatus status, Locale locale) throws SQLException {
-	PreparedStatement statement = connection.prepareStatement("INSERT INTO terms (concept_id,status,lexical_value,lang_id) VALUES (?,?,?,?);");
-	statement.setInt(1, concept.getConceptId());
-	statement.setString(2, status.toString());
-	statement.setString(3, lexicalValue);
-	statement.setString(4, locale.getLanguage());
-	statement.executeUpdate();
-	updateModified(concept);
+	try (PreparedStatement statement = connection.prepareStatement("INSERT INTO terms (concept_id,status,lexical_value,lang_id) VALUES (?,?,?,?);");) {
+	    statement.setInt(1, concept.getConceptId());
+	    statement.setString(2, status.toString());
+	    statement.setString(3, lexicalValue);
+	    statement.setString(4, locale.getLanguage());
+	    statement.executeUpdate();
+	    updateModified(concept);
+	}
     }
 
     public void removeTerm(Term concept, String lexicalValue, Locale locale) throws SQLException {
-	PreparedStatement statement = connection.prepareStatement("DELETE FROM terms WHERE concept_id=? AND lexical_value=? AND lang_id=?;");
-	statement.setInt(1, concept.getConceptId());
-	statement.setString(2, lexicalValue);
-	statement.setString(3, locale.getLanguage());
-	statement.executeUpdate();
-	updateModified(concept);
+	try (PreparedStatement statement = connection.prepareStatement("DELETE FROM terms WHERE concept_id=? AND lexical_value=? AND lang_id=?;");) {
+	    statement.setInt(1, concept.getConceptId());
+	    statement.setString(2, lexicalValue);
+	    statement.setString(3, locale.getLanguage());
+	    statement.executeUpdate();
+	    updateModified(concept);
+	}
     }
 
     public void addRelation(Term concept1, Term concept2, RelationType type) throws SQLException {
-	PreparedStatement statement = connection.prepareStatement("INSERT INTO relationships (concept1, concept2, rel_type) VALUES (?,?,?);");
-	statement.setInt(1, concept1.getConceptId());
-	statement.setInt(2, concept2.getConceptId());
-	statement.setString(3, type.toString());
-	statement.executeUpdate();
-	updateModified(concept1);
-	updateModified(concept2);
+	try (PreparedStatement statement = connection.prepareStatement("INSERT INTO relationships (concept1, concept2, rel_type) VALUES (?,?,?);");) {
+	    statement.setInt(1, concept1.getConceptId());
+	    statement.setInt(2, concept2.getConceptId());
+	    statement.setString(3, type.toString());
+	    statement.executeUpdate();
+	    updateModified(concept1);
+	    updateModified(concept2);
+	}
     }
 
     public void removeRelation(Term concept1, Term concept2, RelationType type) throws SQLException {
-	PreparedStatement statement = connection.prepareStatement("DELETE FROM relationships WHERE concept1 = ? AND concept2 = ? AND rel_type = ?;");
-	statement.setInt(1, concept1.getConceptId());
-	statement.setInt(2, concept2.getConceptId());
-	statement.setString(3, type.toString());
-	statement.executeUpdate();
-	updateModified(concept1);
-	updateModified(concept2);
+	try (PreparedStatement statement = connection.prepareStatement("DELETE FROM relationships WHERE concept1 = ? AND concept2 = ? AND rel_type = ?;");) {
+	    statement.setInt(1, concept1.getConceptId());
+	    statement.setInt(2, concept2.getConceptId());
+	    statement.setString(3, type.toString());
+	    statement.executeUpdate();
+	    updateModified(concept1);
+	    updateModified(concept2);
+	}
     }
 
     public void addMapping(Term source, String target, ExternalVocabulary vocab) throws SQLException {
-	PreparedStatement statement = prepareStatement(
-		"INSERT INTO mappings (source_concept_id, target_concept_id, target_vocabulary_id, mapping_relation) VALUES (?,?,?,?);");
-	statement.setInt(1, source.getConceptId());
-	statement.setString(2, target);
-	statement.setString(3, vocab.toString());
-	statement.setString(4, "close"); // todo: support other mapping relations
-	statement.executeUpdate();
-	updateModified(source);
+	try (PreparedStatement statement = prepareStatement(
+		"INSERT INTO mappings (source_concept_id, target_concept_id, target_vocabulary_id, mapping_relation) VALUES (?,?,?,?);");) {
+	    statement.setInt(1, source.getConceptId());
+	    statement.setString(2, target);
+	    statement.setString(3, vocab.toString());
+	    statement.setString(4, "close"); // todo: support other mapping relations
+	    statement.executeUpdate();
+	    updateModified(source);
+	}
     }
 
     public void removeMapping(Term source, String target, ExternalVocabulary vocab) throws SQLException {
-	PreparedStatement statement = prepareStatement(
-		"DELETE FROM mappings WHERE source_concept_id=? AND target_concept_id=? AND target_vocabulary_id=? AND mapping_relation=?;");
-	statement.setInt(1, source.getConceptId());
-	statement.setString(2, target);
-	statement.setString(3, vocab.toString());
-	statement.setString(4, "close"); // todo: support other mapping relations
-	statement.executeUpdate();
-	updateModified(source);
+	try (PreparedStatement statement = prepareStatement(
+		"DELETE FROM mappings WHERE source_concept_id=? AND target_concept_id=? AND target_vocabulary_id=? AND mapping_relation=?;");) {
+	    statement.setInt(1, source.getConceptId());
+	    statement.setString(2, target);
+	    statement.setString(3, vocab.toString());
+	    statement.setString(4, "close"); // todo: support other mapping relations
+	    statement.executeUpdate();
+	    updateModified(source);
+	}
     }
 
     public void setNote(Term concept, String note) throws SQLException {
-	PreparedStatement statement = prepareStatement("UPDATE concepts SET editorial_note=? WHERE concept_id=?;");
-	statement.setString(1, note);
-	statement.setInt(2, concept.getConceptId());
-	statement.executeUpdate();
-	updateModified(concept);
+	try (PreparedStatement statement = prepareStatement("UPDATE concepts SET editorial_note=? WHERE concept_id=?;");) {
+	    statement.setString(1, note);
+	    statement.setInt(2, concept.getConceptId());
+	    statement.executeUpdate();
+	    updateModified(concept);
+	}
     }
 
     public void setDefinition(Term concept, String definition) throws SQLException {
-	PreparedStatement statement = prepareStatement("UPDATE concepts SET definition=? WHERE concept_id=?;");
-	statement.setString(1, definition);
-	statement.setInt(2, concept.getConceptId());
-	statement.executeUpdate();
-	updateModified(concept);
+	try (PreparedStatement statement = prepareStatement("UPDATE concepts SET definition=? WHERE concept_id=?;");) {
+	    statement.setString(1, definition);
+	    statement.setInt(2, concept.getConceptId());
+	    statement.executeUpdate();
+	    updateModified(concept);
+	}
     }
 
     private void updateModified(Term concept) throws SQLException {
-	PreparedStatement statement = connection.prepareStatement("UPDATE concepts SET modified = CURRENT_TIMESTAMP WHERE concept_id = ?;");
-	statement.setInt(1, concept.getConceptId());
-	statement.executeUpdate();
+	try (PreparedStatement statement = connection.prepareStatement("UPDATE concepts SET modified = CURRENT_TIMESTAMP WHERE concept_id = ?;");) {
+	    statement.setInt(1, concept.getConceptId());
+	    statement.executeUpdate();
+	}
     }
 
     public PreparedStatement prepareStatement(String query) throws SQLException {
